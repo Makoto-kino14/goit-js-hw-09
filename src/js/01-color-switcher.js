0fc00830c548781f43d686f27cd58e70fe78ba5e
Makoto-kino14 <madina14madlen@gmail.com>
2022-10-23 18:19:19 +0600

    1st task - ver 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,28 @@
+import '../css/common.css';
 
+const startBittonEl = document.querySelector('button[data-start]');
+const stopButtonEl = document.querySelector('button[data-stop]');
+const body = document.querySelector('body');
+let interval = null;
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+stopButtonEl.setAttribute('disabled', '');
+
+startBittonEl.addEventListener('click', element => {
+  element.target.setAttribute('disabled', true);
+  stopButtonEl.removeAttribute('disabled');
+
+  interval = setInterval(() => {
+    body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+});
+
+stopButtonEl.addEventListener('click', element => {
+  element.target.setAttribute('disabled', true);
+  startBittonEl.removeAttribute('disabled');
+
+  clearInterval(interval);
+});
